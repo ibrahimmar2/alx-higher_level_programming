6b3cef7b6783a8aa92d9c2906d0506384c3d7eec
root <root@DESKTOP-B6RIHOB>
2024-02-26 17:15:47 +0200

    first

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
+'''
+    Class Rectangle
+'''
 from models.base import Base
-"""Module for Rectangle class"""
 
 
 class Rectangle(Base):
@@ -8,87 +11,130 @@ class Rectangle(Base):
         Inherits from:
             Base
     '''
-    def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
 
+    def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
+        '''
+            Returning private attribute
+        '''
         return self.__width
 
     @width.setter
-    def width(self, data):
-        if not isinstance(data, int):
-            raise TypeError("width must be an integer")
-        if data < 0:
-            raise ValueError(" width must be > 0")
-        self.__width = data
+    def width(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("width", value)
+        self.__width = value
 
     @property
     def height(self):
+        '''
+            Returning private attribute
+        '''
         return self.__height
 
     @height.setter
-    def height(self, data):
-        if not isinstance(data, int):
-            raise TypeError(" height must be an integer")
-        if data < 0:
-            raise ValueError(" height must be > 0")
-        self.__height = data
+    def height(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("height", value)
+        self.__height = value
 
     @property
     def x(self):
+        '''
+            Returning private attribute
+        '''
         return self.__x
 
     @x.setter
-    def x(self, data):
-        if not isinstance(data, int):
-            raise TypeError(" x must be an integer")
-        if data < 0:
-            raise ValueError("x must be > 0")
-        self.__x = data
+    def x(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("x", value)
+        self.__x = value
 
     @property
     def y(self):
+        '''
+            Returning private attribute
+        '''
         return self.__y
 
     @y.setter
-    def y(self, data):
-        if not isinstance(data, int):
-            raise TypeError(" y must be an integer")
-        if data < 0:
-            raise ValueError(" y must be >= 0")
-        self.__y = data
+    def y(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("y", value)
+        self.__y = value
 
     def area(self):
-
-        return (self.__height * self.__width)
+        '''
+            Returns the area of the rectangle
+        '''
+        return (self.height * self.width)
 
     def display(self):
-        print("\n"*self.y, end="")
-        for x in range(self.height):
-            print(" " * self.x, end="")
-            for j in range(self.width):
-                print("#", end="")
-            print()
-
-    def __str__(self):
-        return (
-            "[Rectangle] ({}) {}/{} - {}/{}"
-            .format(self.id, self.__x, self.__y, self.__width, self.__height)
-            )
+        '''
+            Prints to stdout the representation of the rectangle
+        '''
+        rectangle = ""
+        print("\n" * self.y, end="")
+        for i in range(self.height):
+            rectangle += (" " * self.x) + ("#" * self.width) + "\n"
+        print(rectangle, end="")
 
     def update(self, *args, **kwargs):
-        properties = ['id', 'width', 'height', 'x', 'y']
-        if args:
-            for i, arg in enumerate(args):
-                if i < len(properties):
-                    setattr(self, properties[i], arg)
-        else:
-            for key, value in kwargs.items():
-                if key in properties:
-                    setattr(self, key, value)
+        '''
+            Updates the arguments in the class
+        '''
+        if len(args) == 0:
+            for key, val in kwargs.items():
+                self.__setattr__(key, val)
+            return
+        try:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
+        except IndexError:
+            pass
+
+    def to_dictionary(self):
+        '''
+            Returns a dictionary representation of this class
+        '''
+        return {'x': getattr(self, "x"),
+                'y': getattr(self, "y"),
+                'id': getattr(self, "id"),
+                'height': getattr(self, "height"),
+                'width': getattr(self, "width")}
+
+    @staticmethod
+    def setter_validation(attribute, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(attribute))
+        if attribute == "x" or attribute == "y":
+            if value < 0:
+                raise ValueError("{} must be >= 0".format(attribute))
+        elif value <= 0:
+            raise ValueError("{} must be > 0".format(attribute))
+
+    def __str__(self):
+        '''
+            Overwritting the str method
+        '''
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
+                                                       self.width, self.height)
