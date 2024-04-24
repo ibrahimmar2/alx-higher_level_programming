f87ed8de96ba90f1b4f7e9c3af1673e293c73b17
root <root@DESKTOP-B6RIHOB>
2024-04-24 08:52:10 +0200

    first

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (let i = 0; i < 3; i++) {
-  console.log(langs[i]);
+for (const x of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(x);
 }
