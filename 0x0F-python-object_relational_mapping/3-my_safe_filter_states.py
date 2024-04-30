aebc9ca61b8d99ebbf7b4e2a88259152a2b10a7b
root <root@DESKTOP-B6RIHOB>
2024-04-30 08:12:30 +0300

    firsts

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,38 +1,18 @@
 #!/usr/bin/python3
-
-"""
-    A script that lists all states from the database hbtn_0e_0_usa
-    starting with capital letter N
-    Username, password and database names are given as user args
-"""
-
-import sys
+"""Script that takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument, safe from MySQL
+injections"""
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    # Database connection parameters
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Connect to the database
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-    cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states \
-                    WHERE name LIKE BINARY = %s\
-                    ORDER BY id ASC".format(sys.argv[4],))
-
-    results = cursor.fetchall()
-
-    for row in results:
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                (argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
-    cursor.close()
+    cur.close()
     db.close()
